47b5938c92860a854585b9f541930e5056ad41cb
Wallace <wbalaniu@my.centennialcollege.ca>
2019-10-16 16:17:31 -0400

    Restricted player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,12 +4,34 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // Public Variables
     public float speed;
+    public GameObject laser;
+    public Transform laserSpawn;
+    public float fireRate = 0.5f;
+    public Vector2 bottomLeftBoundary;
+    public Vector2 topRightBoundary;
+
+    // Private variables
+    private Rigidbody2D rBody;
+    private float counter = 0.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        counter += Time.deltaTime;
+
+        if(Input.GetButton("Fire1") && counter > fireRate)
+        {
+            // Instantiate the laser
+            Instantiate(laser, laserSpawn.position, laser.transform.rotation);
+            counter = 0.0f;
+        }
     }
 
     // Update is called once per frame
@@ -21,7 +43,11 @@ public class PlayerController : MonoBehaviour
 
         // Debug.Log("Horizontal: " + horiz + " Vertical: " + vert);
 
-        Rigidbody2D rBody = GetComponent<Rigidbody2D>();
         rBody.velocity = new Vector2(horiz * speed, vert * speed);
+
+        // Restrict my player from leaving the play area
+        rBody.position = new Vector2(
+            Mathf.Clamp(rBody.position.x, bottomLeftBoundary.x, topRightBoundary.x),
+            Mathf.Clamp(rBody.position.y, bottomLeftBoundary.y, topRightBoundary.y));
     }
 }
