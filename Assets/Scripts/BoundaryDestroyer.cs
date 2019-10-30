c54b3319ae4aaa02f2a12ddd8a1b63481d1c965e
Wallace <wbalaniu@my.centennialcollege.ca>
2019-10-30 15:27:29 -0400

    Added boundary

---
+++ Assets/Scripts/BoundaryDestroyer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundaryDestroyer : MonoBehaviour
+{
+    // Runs once
+    // When another collider leaves this colliders area
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Destroy(other.gameObject);
+    }
+
+    // Runs once
+    // When another collider enters this colliders area
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        
+    }
+
+    // Runs every frame if...
+    // Another collider stay inside this colliders area
+    void OnTriggerStay2D(Collider2D other)
+    {
+        
+    }
+}
