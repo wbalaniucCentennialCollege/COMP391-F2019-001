ded750ee4853416e28b5d6b49330dfcd482639d7
Wallace <wbalaniu@my.centennialcollege.ca>
2019-11-06 15:15:45 -0500

    Asteroid destruction and explosion spawn

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour
+{
+    public GameObject explosion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        // Instantiate an explosion and position it
+        Instantiate(explosion, this.transform.position, this.transform.rotation);
+
+        Destroy(other.gameObject); // "Laser" or ship or anything with a collider
+        Destroy(this.gameObject); // Asteroid
+    }
+}
